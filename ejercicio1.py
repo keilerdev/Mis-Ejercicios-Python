e2895f96c88a840d1a21fa67c160d78904bcb4b7
keilerdev <keilersalcedo123@gmail.com>
2026-01-20 18:22:28 -0400

    Día 1: Mi primera calculadora

---
+++ ejercicio1.py
@@ -0,0 +1,24 @@
+numero1 = float(input("ingresa un número entero: "))
+numero2 = float(input("ingresa otro número entero: "))
+print("\n ")
+
+print("=" * 15)
+print("CALCULADORA".center(15))
+print("=" * 15)
+
+print("\nnumero 1:", numero1)
+print("\nnumero 2:", numero2)
+
+print("-" * 15)
+
+suma = numero1 + numero2
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+division = numero1 / numero2
+
+
+print("\nSuma:", suma)
+print("\nResta:", resta)
+print("\nMultiplicación:", multiplicacion)
+print("\nDivisión:", division)
+print("=" * 15)
